76c86deddd92c97673f452b8087e6f9a51722633
Koby <mcguirej2019@gmail.com>
2022-10-31 18:43:46 -0400

    Finished Project

--- java-file.js
+++ java-file.js
@@ -1,42 +1,70 @@
-
-
 function getComputerChoice() {
-    let arrRandom = ["Rock", "Paper", "Scissors"]
-    let pick = arrRandom[Math.floor((Math.random()) * 3)]
-    return pick;
-  }
-  
-  function playGame(playerSelection, computerSelection) {
-    playerLow = playerSelection.toLowerCase()
-    computerLow = computerSelection.toLowerCase()
-    // If you tie with the machine, the score returned will be zero, if you win, it will be one, and if you lose, it will be negative one.
-    let winner = (playerLow === computerLow) ? 0 : (playerLow === "rock" && computerLow === 
-    "scissors") ? 1 : (playerLow === "paper" && computerLow === "rock") ? 1 : 
-    (playerLow === "scissors" && computerLow === "paper") ? 1 : -1
-    return winner
-  }
-  
-  function game () {
-    // Array that will be keeping track of wins/losses through each round
-    let champ = []
+  let arrRandom = ["rock", "paper", "scissors"]
+  let pick = arrRandom[Math.floor((Math.random()) * 3)]
+  return pick;
+}
 
-    // Loop that will actually play the game and grab a winner for each round
-    for (i = 0; i < 5; i++) {
-      let playerSelection = prompt("Rock, Paper, or Scissors?");
-      let winner = playGame(playerSelection, getComputerChoice());
-      champ.push(winner);
-      }
-      
-    // Loop iteration in order to determine how many round the player won out of the five played
-    sum = 0;
-    for (i = 0; i < 5; i++) {
-        if (champ[i] === 1) {
-            sum += 1;
+
+function playGame(playerLow, computerLow) {
+  playerLow = playerLow.toLowerCase();
+  console.log(playerLow);
+  console.log(computerLow);
+  let winner = (playerLow === computerLow) ? "Tie!" : (playerLow === "rock" && computerLow === 
+  "scissors") ? "You win!" : (playerLow === "paper" && computerLow === "rock") ? "You win!" : 
+  (playerLow === "scissors" && computerLow === "paper") ? "You win!" : "You lose!"
+  return winner;
+}
+
+
+let buttons = document.querySelectorAll(".button");
+let human_div = document.querySelector(".humanScore");
+let computer_div = document.querySelector(".computerScore");
+let content = document.querySelector(".content");
+let gameOver = false;
+let winner = ""
+let playerSelection = ""
+let humanPoints = 0;
+let computerPoints = 0;
+
+human_div.innerHTML = `0`;
+computer_div.innerHTML = `0`;
+content.innerHTML = "No round has been played yet!";
+
+console.log("Please Click a box: ")
+
+for (const button of buttons) {
+button.addEventListener("click", function onClick() {
+  if (gameOver === false) {
+    playerSelection = button.textContent;
+    console.log("Box Clicked");
+    winner = playGame(playerSelection, getComputerChoice());
+    console.log(winner)
+    if (winner === "You win!") {
+      content.innerHTML = `${winner}`;
+      humanPoints++;
+    }
+    else if (winner === "You lose!") {
+      content.innerHTML = `${winner}`;
+      computerPoints++;
+    }
+    else {
+      content.innerHTML = `${winner}`;
+    }
+    console.log("Human: " + humanPoints);
+    console.log("Computer: " + computerPoints);
+    human_div.innerHTML = `${humanPoints}`;
+    computer_div.innerHTML = `${computerPoints}`;
+    //Checking gameOver Variable to see if one side has reached five points, update text to show that the game is over if one side has won.
+    if (computerPoints === 5 || humanPoints === 5) {
+        gameOver = true;
+        //Displaying a losing / winning message that will be displayed in "content" based on the scores
+        if (computerPoints === 5) {
+          content.innerHTML = "You have lost!";
+        }
+        else {
+          content.innerHTML = "You have won!";
         }
     }
-    
-    // Telling the player how many rounds he/she won out of five
-    console.log(`You won ${sum} games out of five!`);
   }
-  
-  game()
+})
+}
